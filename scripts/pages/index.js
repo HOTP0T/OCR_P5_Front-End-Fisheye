08822ed0b0347d467130a7f244037bf4a0570bda
HOTPOT <tpcmaky@gmail.com>
2024-04-01 22:17:57 +0200

    added photographer assetes in local files

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,27 +1,26 @@
 async function getPhotographers() {
-    // Récupération des photographes depuis le fichier JSON
-    const response = await fetch('./data/photographers.json');
-    const photographe = await response.json();
-    console.log(photographe);
+  // Récupération des photographes depuis le fichier JSON
+  const response = await fetch('./data/photographers.json');
+  const photographe = await response.json();
+  console.log(photographe);
 }
 console.log(getPhotographers());
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+async function displayData(photographers) {
+  const photographersSection = document.querySelector(".photographer_section");
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerTemplate(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    }
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerTemplate(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
+}
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        // Affiche les photographes
-        displayData(photographers);
-    }
-    
-    init();
-    
+async function init() {
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+  // Affiche les photographes
+  displayData(photographers);
+}
+
+init();
