57f504bdf7faa1006870beb527e513f52be8acf4
HOTPOT <tpcmaky@gmail.com>
2024-04-04 16:36:48 +0200

    added json files depending on how fetch

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,18 +1,4 @@
-// TODO ADD JSOC/**
-/* Description placeholder
-* /
-function displayModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "block";
-}
-
-// TODO ADD JSOC
-/**
- * Description placeholder
- */
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
-
-// trial 
