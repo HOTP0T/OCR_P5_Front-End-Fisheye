6605366700efa2a878423ca499bfb4ad82c69f5f
HOTPOT <tpcmaky@gmail.com>
2024-04-06 14:25:07 +0200

    fixed form and main content opacity switch when modalDisplay // improved css

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,11 @@
 function displayModal() {
   const modal = document.getElementById("contact_modal");
+  document.body.classList.add("dimmed");
   modal.style.display = "block";
 }
 
 function closeModal() {
   const modal = document.getElementById("contact_modal");
+  document.body.classList.remove("dimmed");
   modal.style.display = "none";
 }
